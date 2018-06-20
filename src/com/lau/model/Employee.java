c4cb949be27f99bb5a23e97e8eba41890a4433fb
Mike <Mike@DESKTOP-5NAUPGM.fios-router.home>
2018-06-19 23:36:09 -0400

    added few member variables

--- src/com/lau/model/Employee.java
+++ src/com/lau/model/Employee.java
@@ -1,5 +1,27 @@
 package com.lau.model;
 
 public class Employee {
+	private String lastName;
+	private String firstName;
+	private String empId;
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
 
 }
